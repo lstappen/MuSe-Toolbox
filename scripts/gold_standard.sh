3d42a5b828d80467a25ce189fa7a30e191f43e95
leaschumann <lea.schumann@student.uni-augsburg.de>
2021-04-03 16:25:30 +0200

    example gold standard

--- scripts/gold_standard.sh
+++ scripts/gold_standard.sh
@@ -1,3 +1,45 @@
 #!/bin/sh
 
+export PYTHONUNBUFFERED=1
 
+# Fusing several annotations to one Gold Standard using different methods
+
+input=examples/raw  # path to input raw annotations
+
+emotion_dim=arousal  # emotion dimention which is to be fused, annotations must be in 'arousal' folder
+pre_smooth_filter=savgol  # smooth the individual annotations with a Savitzky-Golay filter
+pre_smoothing_window=5  # the window size of the Savitzky-Golay filter
+post_smoothing_window=15  # smoothing window of the filter used on the fused annotation
+annotators="1 2 3"  # annotator ids whose annotations are to be fused
+
+######################
+
+output_unaligned="output/std_all_samples_${pre_smooth_filter}_${pre_smoothing_window}_${post_smoothing_window}_unaligned"  # path where the fused annotations are to be saved
+
+# 1. Fusion using mean
+fusion=mean
+python src/muse-toolbox gold_standard -inp $input -out $output_unaligned --std_annos_all_samples --fusion $fusion -dim $emotion_dim  --pre_smoothing $pre_smooth_filter --pre_smoothing_window $pre_smoothing_window --post_smoothing_window $post_smoothing_window --annotators $annotators --ts timestamp --plot &>> out.txt
+
+# 2. Fusion using EWE
+fusion=ewe
+python src/muse-toolbox gold_standard -inp $input -out $output_unaligned --std_annos_all_samples --fusion $fusion -dim $emotion_dim  --pre_smoothing $pre_smooth_filter --pre_smoothing_window $pre_smoothing_window --post_smoothing_window $post_smoothing_window --annotators $annotators --ts timestamp --plot &>> out.txt
+
+# 3. Fusion using DBA
+fusion=dba
+python src/muse-toolbox gold_standard -inp $input -out $output_unaligned --std_annos_all_samples --fusion $fusion -dim $emotion_dim  --pre_smoothing $pre_smooth_filter --pre_smoothing_window $pre_smoothing_window --post_smoothing_window $post_smoothing_window --annotators $annotators --ts timestamp --plot &>> out.txt
+
+######################
+
+output_aligned="output/std_all_samples_${pre_smooth_filter}_${pre_smoothing_window}_${post_smoothing_window}_aligned"  # path where the fused annotations are to be saved
+
+# 4. Alignment with CTW and fusion using EWE (AWE)
+fusion=ewe
+alignment=ctw
+python src/muse-toolbox gold_standard -inp $input -out $output_aligned --std_annos_all_samples --alignment $alignment --fusion $fusion -dim $emotion_dim  --pre_smoothing $pre_smooth_filter --pre_smoothing_window $pre_smoothing_window --post_smoothing_window $post_smoothing_window --annotators $annotators --ts timestamp --plot &>> out.txt
+
+
+######################
+
+# Plot fusions
+fusion_output_paths="$output_unaligned $output_aligned"
+python scripts/plot_gs.py --paths $fusion_output_paths &>> out.txt
---
+++ scripts/plot_gs.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+from os import listdir
+import glob
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--paths', nargs='+')
+params = parser.parse_args()
+
+data_path = 'examples/raw/arousal'
+
+for file in listdir(data_path):
+    emo = data_path.split('/')[-1]
+    df = pd.read_csv(os.path.join(data_path, file))
+
+    timestamps = df['timestamp'].values
+
+    plt.figure()
+    # plot raw annotations
+    for anno in ['1', '2', '3']:
+        plt.plot(timestamps, df[anno].values, color="gray")
+    # plot fused annotations
+    for path in params.paths:
+        for fusion in glob.glob(path + '*/*'):
+            fusion = fusion.replace("\\", '/')
+            print(fusion)
+            if 'unaligned' not in fusion and 'ctw' in fusion:  # no fusion only aligned annotations
+                continue
+            elif 'unaligned' not in fusion and 'ewe' in fusion:
+                print('awe')
+                name = 'awe'
+            elif 'unaligned' not in fusion and 'mean' in fusion:
+                name = 'ctw+mean'
+            else:
+                name = fusion.split('/')[-1]
+            fusion_df = pd.read_csv(os.path.join(fusion, emo, file))
+            plt.plot(timestamps, fusion_df['value'].values, label=name.upper(), linewidth=2.0)
+
+    plt.xlabel('time (s)')
+    plt.legend(loc="upper right")
+    plt.ylim([-1, 1])
+
+    # plt.show()
+    plt.savefig(os.path.join('output', file.split('.')[0] + '.png'))
+
+    plt.close()
